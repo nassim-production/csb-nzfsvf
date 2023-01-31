5f91f0b7b3d67532c81b124fabf761c88eb76979
Code Sandbox <hello@codesandbox.io>
2023-01-31 23:01:05 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.a2061327.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmqtt-chat-app"]=this["webpackJsonpmqtt-chat-app"]||[]).push([[0],{12:function(t,e,c){},14:function(t,e,c){"use strict";c.r(e);var n=c(1),s=c(3),o=(c(12),c(0));function r(){return Object(o.jsxs)("div",{className:"App",children:[Object(o.jsx)("h1",{children:"Hello CodeSandbox"}),Object(o.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var a=document.getElementById("root");Object(s.createRoot)(a).render(Object(o.jsx)(n.StrictMode,{children:Object(o.jsx)(r,{})}))}},[[14,1,2]]]);
+//# sourceMappingURL=main.a2061327.chunk.js.map
